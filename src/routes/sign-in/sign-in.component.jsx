9bc0fae09f82840b594db77b9578c56de0351620
A.Karwowski <akk42@zips.uakron.edu>
2022-06-28 21:17:25 -0600

    format

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,15 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils"
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 const SignIn = () => {
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
-        console.log(response)
-    }
+        console.log(response);
+    };
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In With Google Popup</button>
         </div>
-    )
-}
+    );
+};
 
-export default SignIn
+export default SignIn;
