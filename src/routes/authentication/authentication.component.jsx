749d8676d40547096407b83dd54b66caeb4eccdd
A.Karwowski <akk42@zips.uakron.edu>
2022-07-05 21:29:26 -0600

    add styles for auth page

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,16 +1,13 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
-import Button from "../../components/button/button.component"
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
+import "./authentication.styles.scss"
+
 const Authentication = () => {
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    };
 
     return (
-        <div>
-            <h1>Sign In Page</h1>
-            <Button buttonType="google" onClick={logGoogleUser}>Sign In With Google Popup</Button>
+        <div className="authentication-container">
+            <SignInForm/>
             <SignUpForm />
         </div>
     );
