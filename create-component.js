35224e0a8cc22bc266db3ac1a0c30686fd17a3a3
A.Karwowski <akk42@zips.uakron.edu>
2022-07-12 13:06:03 -0600

    update create-component.js

--- create-component.js
+++ create-component.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-readline.question(`What is the name of the component you wish to create: `, name => {
-    console.log(`Creating ${name} directory`)
-    const comp = path.join(__dirname,`/src/components/${name}`)
+readline.question(`What is the name of the component you wish to create: `, (name) => {
+    console.log(`Creating ${name} directory`);
+    const comp = path.join(__dirname, `/src/components/${name}`);
     fs.mkdir(comp, (err) => {
         if (err) {
             throw err;
@@ -16,6 +16,19 @@ readline.question(`What is the name of the component you wish to create: `, name
     console.log(`Creating ${name}.styles.scss file`);
     fs.writeFileSync(comp + `/${name}.styles.scss`, "");
     console.log(`Creating ${name}.components.jsx file`);
-    fs.writeFileSync(comp + `/${name}.components.jsx`, `import './${name}.styles.scss'`);
+    fs.writeFileSync(
+        comp + `/${name}.components.jsx`,
+        `
+import './${name}.styles.scss'
+const ${name[0].toUpperCase() + name.slice(1)} = () => {
+    return (
+        <div>
+            placeholder
+        </div>
+    )
+}
+
+export default ${name[0].toUpperCase() + name.slice(1)}
+`);
     process.exit(0);
-})
+});
