19f7c0a8a4cdd3cf2a7a416c176ee1c78a6aee07
A.Karwowski <akk42@zips.uakron.edu>
2022-07-12 12:57:27 -0600

    create component function

---
+++ create-component.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question(`What is the name of the component you wish to create: `, name => {
+    console.log(`Creating ${name} directory`)
+    const comp = path.join(__dirname,`/src/components/${name}`)
+    fs.mkdir(comp, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    console.log(`Creating ${name}.styles.scss file`);
+    fs.writeFileSync(comp + `/${name}.styles.scss`, "");
+    console.log(`Creating ${name}.components.jsx file`);
+    fs.writeFileSync(comp + `/${name}.components.jsx`, `import './${name}.styles.scss'`);
+    process.exit(0);
+})
